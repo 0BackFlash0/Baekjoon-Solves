a9a4a507af4e718cefa6d53fd2593fdc0d301ae5
BackFlash <53032619+0BackFlash0@users.noreply.github.com>
2022-12-06 09:19:21 +0900

    12-06

--- test.py
+++ test.py
@@ -1,34 +1,53 @@
-def step_map(maze):
-    max_r = len(maze)
-    max_c = len(maze[0])
 
-    step_matrix = [[0 if maze[r][c] == 1 else -1 for c in range(max_c)] for r in range(max_r)]
-    step_matrix[0][0] = 1
-    pos = [(0, 0)]
+from copy import deepcopy
 
-    while(len(pos)>0):
-        r, c = pos.pop(0)
+def DFS(_graph_list, start):
+    graph_list = deepcopy(_graph_list)
 
-        close_pos = [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]
+    search_list = [start]
+    result = []
+    while(len(search_list)>0):
+        current = search_list.pop()
+        current_node = graph_list[current]
+        if(not current_node[0]):
+            current_node[0] = True
+            result.append(current+1)
+            for next_node in sorted(current_node[1], reverse=True):
+                if(graph_list[next_node][0]==False):
+                    search_list.append(next_node)
 
-        for _r, _c in close_pos:
-            if(0<=_r<max_r and 0<=_c<max_c and step_matrix[_r][_c]==0):
-                pos.append((_r, _c))
-                step_matrix[_r][_c] = step_matrix[r][c] + 1
-        
-            # print("\n".join(list(map(str, step_matrix))))
-            # print()
-    
-    return step_matrix
+    return result
+
+def BFS(_graph_list, start):
+    graph_list = deepcopy(_graph_list)
+    search_list = [start]
+    result = []
+    while(len(search_list)>0):
+        current = search_list.pop(0)
+        current_node = graph_list[current]
+        if(not current_node[0]):
+            current_node[0] = True
+            result.append(current+1)
+            for next_node in sorted(current_node[1]):
+                if(graph_list[next_node][0]==False):
+                    search_list.append(next_node)
+
+    return result
 
 
+n, m, start = map(int, input().split())
 
-row, column = map(int, input().split())
-maze = []
-for _ in range(row):
-    maze.append(list(map(int, input())))
+graph_list = [[False, []] for _ in range(n)]
+# 현재 값 (인덱스)
+# 방문 여부 (True/False)
+# 다음 방문 (인덱스들)
 
-# print("\n".join(list(map(str, maze))))
-# print()
-# print(step_map(maze))
-print(step_map(maze)[row-1][column-1])
+for _ in range(m):
+    s, e = map(int, input().split())
+
+    graph_list[s-1][1].append(e-1)
+    graph_list[e-1][1].append(s-1)
+    # 인덱스로 계산하기 때문에 1씩 제외하고 
+
+print(" ".join(map(str, DFS(graph_list, start-1))))
+print(" ".join(map(str, BFS(graph_list, start-1))))
