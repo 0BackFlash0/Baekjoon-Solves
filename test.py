39dc274eedf3942d3f08f5e0bf0e9535ce9308e2
BackFlash <53032619+0BackFlash0@users.noreply.github.com>
2022-12-12 03:43:14 +0900

    12-12

--- test.py
+++ test.py
@@ -1,37 +1,24 @@
 import math
-# import sys
-# input = sys.stdin.buffer.readline
 
-##평균 길이 구하기.
+def clear_snow(_snow_list):
+    snow_list = _snow_list
 
-##모자란 길이 구하기.
+    max_snow = max(snow_list)
+    rest_snow = sum(snow_list)-max_snow
+    result = 0
 
-def div_lan(lan_list, n, k):
-    lan_list.sort(reverse=True)
+    if(max_snow>rest_snow):
+        result = max_snow
+    else:
+        result = math.ceil((rest_snow - max_snow)/2) + max_snow
 
-    start_divisor = math.ceil(k/n)
-    #현재까지 가능한 pivot값들 중 가장 큰 값
-    start_pivot = lan_list[0]//math.ceil(k/n)
-    max_pivot = 0
-    for lan in lan_list:
-        #start_divisor에서부터 k+1까지 반복한다.
-        if(lan//start_pivot==0):
-            break
-        for div in range(lan//start_pivot, k+1):
-            pivot = lan//div
-            total_lan = sum(map(lambda s : s//pivot, lan_list))
-            if(total_lan==k):
-                return pivot
-            elif(total_lan>k):
-                if(max_pivot<pivot):
-                    max_pivot = pivot
-                    start_pivot = max_pivot
-                break
-    return max_pivot
-    
-    
+    if(result>1440):
+        return -1
+    else:
+        return result
 
-n, k = map(int, input().split())
-lan_list = [int(input()) for _ in range(n)]
 
-print(div_lan(lan_list, n, k))
+n = int(input())
+snow_list = list(map(int, input().split()))
+
+print(clear_snow(snow_list))
