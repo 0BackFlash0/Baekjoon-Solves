7f231ed2bae0173fb9fb47e06a359f10de09f089
BackFlash <53032619+0BackFlash0@users.noreply.github.com>
2022-12-15 09:22:51 +0900

    12-15

--- test.py
+++ test.py
@@ -1,19 +1,45 @@
-def clockwise(a, b, c):
-    v1 = [a[0]-b[0], a[1]-b[1]]
-    v2 = [c[0]-b[0], c[1]-b[1]]
+# 현재 무게가 n일때의 최대값 저장
+# 그거보다 작으면 무효
+# 합들보다 이전 값이 더 크면 그것도 무효
+# weight-[value] 딕셔너리
 
-    cross_product_z = v1[0]*v2[1] - v1[1]*v2[0]
+def bag_capacity(weight_value_dict, max_weight):
+    
+    keys = weight_value_dict.keys()
+    memorize = [[0, [{key:0 for key in keys}]]]
 
-    if(cross_product_z>0):
-        return -1
-    elif(cross_product_z==0):
-        return 0
+    for i in range(1, max_weight+1):
+        tmp_memorize = memorize[i-1]
+        key_list = key.copy()
+            if(i-key>=0):
+                for memo_dict in memorize[i-key][1]:
+                    if(memorize[i-key][1][key]>=len(weight_value_dict[key])):
+                        continue
+                    value = memorize[i-key][0] + weight_value_dict[key][memorize[i-key][1][key]]
+                    if(value>tmp_memorize[0]):
+                        tmp_memorize = [value, memorize[i-key][1].copy()]
+                        tmp_memorize[1][key] += 1
+        print(tmp_memorize)
+        memorize.append(tmp_memorize)
+
+
+    return memorize[max_weight][0]
+
+
+
+    
+n, max_weight = map(int, input().split())
+weight_value_dict = dict()
+
+for i in range(n):
+    w, v = map(int, input().split())
+    if(w not in weight_value_dict.keys()):
+        weight_value_dict[w] = [v]
     else:
-        return 1
+        idx = 0
+        while(idx<len(weight_value_dict[w]) and v<weight_value_dict[w][idx]):
+            idx += 1
+        weight_value_dict[w].insert(idx ,v)
+        
 
-
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-c = list(map(int, input().split()))
-
-print(clockwise(a, b, c))
+print(bag_capacity(weight_value_dict, max_weight))
