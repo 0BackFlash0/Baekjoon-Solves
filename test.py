8dfd02d331ee28a057edf7889d3f8b77d6224754
0BackFlash0 <kbj2kbj22@naver.com>
2023-01-06 17:33:45 +0900

    01-06

--- test.py
+++ test.py
@@ -1,30 +1,26 @@
+
 import sys
 input = sys.stdin.readline
-import math
+
+def partial_sum(n, s, num_list):
+    
+    num_list = num_list
+    num_list.sort(reverse=True)
+
+    sum = 0
+    l = 0
+
+    while(l<n):
+        sum += num_list[l]
+        l += 1
+
+        if(sum>=s):
+            return l
+    
+    return 0
 
 
-def find_prime_2n(n):
+n,s = map(int, input().split())
 
-    is_prime_list = [1 for _ in range(2*n + 1)]
-    is_prime_list[0] = 0
-    is_prime_list[1] = 0
-
-    for i in range(2, int(math.sqrt(2*n))+1):
-
-        if(is_prime_list[i]==1):
-
-            for not_prime in range(i*i, 2*n+1, i):
-                is_prime_list[not_prime] = 0
-
-    return sum(is_prime_list[n+1:2*n+1])
-
-
-while True:
-    n = int(input())
-    if(n==0): break
-    print(find_prime_2n(n))
-
-
-
-
-
+num_list = list(map(int, input().split()))
+print(partial_sum(n, s, num_list))
