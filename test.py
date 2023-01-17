d4acc8507c4f631e911363180907032946d4700f
0BackFlash0 <kbj2kbj22@naver.com>
2023-01-17 19:09:11 +0900

    01-17

--- test.py
+++ test.py
@@ -1,39 +1,46 @@
 import sys
 input = sys.stdin.readline
 
-class Node:
-    def __init__(self):
-
-        self.parent = None
-
-    def setParent(self, parent):
-        self.parent = parent
+def N_queen(n):
     
-    def getRoot(self, compress=True):
-        if(self.parent==None):
-            return self
-        
-        result = self.parent.getRoot()
-        if(compress):
-            self.setParent(result)
+    board = [0] * n
+    result_list = [0 for _ in range((n-1)//2+1)]
+    idx = 0
 
-        return result
-
+    def recursive_queen(level):
+        if(level==n):
+            result_list[idx] += 1
+            return
 
 
-n, m = map(int, input().split())
+        for row in range(n):
+            board[level] = row
+            is_available = True
 
-nodes = [Node() for i in range(n+1)]
+            for lv in range(level):
+                if(board[level] == board[lv] or abs(board[level] - board[lv]) == level - lv):
+                    is_available = False
+                    break
+            
+            if(is_available):
+                recursive_queen(level+1)
+    
+    # 절반만 계산한 뒤, 나머지는 곱해서 계산
+    #체스판의 절반만큼 순회
+    for x in range((n-1)//2+1):
+        idx = x
+        board[0] = x
+        recursive_queen(1)
 
-for _ in range(m):
-    oper, a, b = map(int, input().split())
-
-    a_root = nodes[a].getRoot()
-    b_root = nodes[b].getRoot()
-    is_union = b_root == a_root
-    if(oper==0):
-        if(not is_union): 
-            b_root.setParent(a_root)
+    if(n%2==0):
+        return sum(result_list) * 2
     else:
-        print("YES" if is_union else "NO")
-    
+        return sum(result_list[:-1]) * 2 + result_list[-1]
+
+
+
+n = int(input())
+print(N_queen(n))
+        
+
+
