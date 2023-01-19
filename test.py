82e3c7fd5c928b170eda38383a8bc6772dd689b5
0BackFlash0 <kbj2kbj22@naver.com>
2023-01-19 17:37:02 +0900

    01-19

--- test.py
+++ test.py
@@ -1,48 +1,11 @@
 import sys
 input = sys.stdin.readline
 
-def gcd(_a, _b):
+class sudoku_group:
+    
 
-    a = _a
-    b = _b
+sudoku = []
+groups = {"row" : [], "col" : [], "sq" : []}
 
-    if(b>a):
-        b = _a
-        a = _b
-
-    if(a%b==0):
-        return b
-    else:
-        return gcd(b, a%b)
-
-def get_div(n):
-    divisors = []
-    divisors_back = []
-
-    for i in range(1, int(n**(1/2))+1):
-        if(n%i == 0):
-            divisors.append(i)
-            if(i!=n//i):
-                divisors_back.append(n//i)
-
-    return divisors + divisors_back[::-1]
-
-
-n = int(input())
-
-# 초기 2개값에 대해 계산
-nums = []
-nums.append(int(input()))
-nums.append(int(input()))
-available_gcd = abs(nums[-2]-nums[-1])
-
-for _ in range(n-2):
-    nums.append(int(input()))
-    available_gcd = gcd(available_gcd, abs(nums[-2]-nums[-1]))
-    # print(available_gcd, abs(nums[-2]-nums[-1]))
-        
-
-divs = get_div(available_gcd)
-
-print(" ".join(map(str, divs[1:])))
-
+for _ in range(9):
+    sudoku.append( list(map(int, input().split())) )
