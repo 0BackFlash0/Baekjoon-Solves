d3b725e4f20c650a3e46e872456e4975d4225d3e
BackFlash <53032619+0BackFlash0@users.noreply.github.com>
2023-02-08 05:08:08 +0900

    02-08

--- test.py
+++ test.py
@@ -1,84 +1,73 @@
 import sys
+
 input = sys.stdin.readline
-
-def available_set(r, c, box):
-
-    r_set = groups["row"][r].leak
-    c_set = groups["row"][c].leak
-    box_set = groups["row"][box].leak
-
-    result = r_set.intersection(c_set).intersection(box_set)
-
-    return result
-
-class sudoku_node:
-    
-    def __init__(self, r_group, c_group, box_group):
-        self.r = r_group
-        self.c = c_group
-        self.box = box_group
-
-    def get_available_list(self, target="all"):
-        if(target=="row"):
-            return self.r.numbers
-        elif(target=="col"):
-            return self.c.numbers
-        elif(target=="box"):
-            return self.box.numbers
-        elif(target=="all"):
-            return [r_bool and c_bool and box_bool for r_bool, c_bool, box_bool in zip(self.r, self.c, self.box)]
-
-class sudoku_group:
-    
-    def __init__(self):
-        self.numbers = [True, True, True, True, True, True, True, True, True]
-        self.empties = []
-
-    def add_empty(self, node):
-        self.empties.append(node)
-
-    def add_available(self, num):
-        self.numbers[num-1] = False
+sys.setrecursionlimit(10000)
 
 
-    
 
-# 각 클래스에서 부족한 수 목록
-# 노드 - 각 클래스 연결
+def sudoku(input_list, r_group, c_group, b_group):
 
-# 클래스 -> 부족한 요소들 + 노드들
-# 노드 -> 클래스
+    result_list = []
+    input_list = input_list
+    r_group = r_group
+    c_group = c_group
+    b_group = b_group
 
-empties = []
+    def check_availbable():
+        if(len(input_list)==0):
+            return True
 
-groups = {"row" : [], "col" : [], "box" : []}
+        r, c, b = input_list[-1]
+
+        for i in range(9):
+
+            if(r_group[r][i] and c_group[c][i] and b_group[b][i]):
+
+                r_group[r][i] = False
+                c_group[c][i] = False
+                b_group[b][i] = False
+                input_list.pop()
+
+                if(check_availbable()):
+                    result_list.append((r, c, i))
+                    return True
+
+                r_group[r][i] = True
+                c_group[c][i] = True
+                b_group[b][i] = True
+                input_list.append((r, c, b))
+
+        return False
+
+    check_availbable()
+    return result_list
+
+
+r_group = [[True for _ in range(9)] for _ in range(9)]
+c_group = [[True for _ in range(9)] for _ in range(9)]
+b_group = [[True for _ in range(9)] for _ in range(9)]
+
+result_matrix = [[0 for _ in range(9)] for _ in range(9)]
+input_list = []
 
 for r in range(9):
-    for c, num in enumerate(map(int, input().split())):
-        box = 3*(r//3) + c//3
+    for c, n in enumerate(map(int, input().split())):
+        b = 3 * (r//3) + c//3
         
-        if(num!=0):
-            groups["row"][r].add_available(num)
-            groups["col"][c].add_available(num)
-            groups["box"][box].add_available(num)
+        result_matrix[r][c] = n
+        if(n==0):
+            input_list.append((r, c, b))
         else:
-            groups["row"][r].add_empty(num)
-            groups["col"][c].add_empty(num)
-            groups["box"][box].add_empty(num)
-            empties.append((r, c, box))
+            r_group[r][n-1] = False
+            c_group[c][n-1] = False
+            b_group[b][n-1] = False
 
+results = sudoku(input_list, r_group, c_group, b_group)
 
-while empties:
+for result in results:
+    r, c, n = result
 
-    empty = empties.pop()
-    empty_set = available_set(**empty)
+    result_matrix[r][c] = n+1
 
-    if(len(empty_set)==1):
-        val = tuple(empty_set)[0]
-        groups["row"][empty[0]].add_available(val)
-        groups["col"][empty[1]].add_available(val)
-        groups["box"][empty[2]].add_available(val)
-
-    elif(len(empty_set)>2):
-        
-
+for r in result_matrix:
+    print(" ".join(map(str, r)))
